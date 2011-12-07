dfbc007af583bc73cd78456eb8926ee8ce1acc1b
tunailgaz <tunailgaz@gmail.com>
2011-12-07 03:42:10 +0000

    path added

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= '';
+$config['base_url']	= 'https://code.google.com/p/student-grading-system/source/browse/trunk/';
 
 /*
 |--------------------------------------------------------------------------
